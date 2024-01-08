ee68d34f9b1db10b08d2bf1d1fcb15cbd1fa84bb
caua-dev-coder <cauatavellaprofissional@gmail.com>
2024-01-08 16:35:39 -0300

    Adição de mensagem alerta caso o número min seja maior que o max

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ function generateNumber() {
 
   const min = parseInt(document.getElementById("input-min").value, 10)
   const max = parseInt(document.getElementById("input-max").value, 10)
-  
- 
   const numberResult = Math.floor(Math.random() * (max - min + 1)) + min;
   
-
-  document.getElementById("resultado").innerHTML = numberResult;
-  document.getElementById("textResult").innerHTML = "O Sortudo do dia é:"
-
+  if ( min <= max ) {
+    document.getElementById("resultado").innerHTML = numberResult;
+    document.getElementById("textResult").innerHTML = "O Sortudo do dia é:"
+  } else {
+    alert ("O Primeiro número deve ser menor que o Segundo")
+  }
 }
